063ec1627a88c7aa9583535dd90261142a7c0c22
JekaDeka <SpecialForWarcraft@gmail.com>
2019-11-21 16:28:55 +0300

    Add frontend application

---
+++ frontend/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="primary" dark>
+      <v-spacer />
+      <v-toolbar-title>Frontend application</v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <v-card>
+              <TransactionTable />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer color="primary" app>
+      <v-spacer />
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import TransactionTable from '@/components/TransactionTable'
+
+export default {
+  name: 'App',
+  components: {
+    TransactionTable
+  },
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/TransactionTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="transcation"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      headers: [
+        { text: 'Transcation title', value: 'title' },
+        { text: 'Amount', value: 'amount' }
+      ],
+      transcation: [
+        {
+          title: 'Заправка "Лукоил"',
+          amount: '1562.2 руб'
+        },
+        {
+          title: 'Магазин "ZHARA"',
+          amount: '5251.53 руб'
+        },
+        {
+          title: 'Шоколадница',
+          amount: '320 руб'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
