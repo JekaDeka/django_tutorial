be5f2ad95dc0d8f318321be1c67e1920653ce971
JekaDeka <SpecialForWarcraft@gmail.com>
2019-11-21 16:37:52 +0300

    Linter Fixes

--- frontend/src/api/auth.service.js
+++ frontend/src/api/auth.service.js
@@ -1,14 +1,14 @@
 import ApiService from './api.service.js'
 
 const AuthService = {
-  login(payload) {
+  login (payload) {
     return ApiService.post('token', payload)
   },
-  verifty_token(payload) {
+  verifty_token (payload) {
     return ApiService.post('token/verify', payload)
   },
-  refresh_token(payload) {
+  refresh_token (payload) {
     return ApiService.post('token/refresh', payload)
-  },
+  }
 }
 export default AuthService
